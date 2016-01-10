b2651504fb4dd1a11372f2045aeaef5226db1ad9
qiufangzhang <904208360@qq.com>
2016-01-10 15:45:36 +0800

    更新

--- zq2.cpp
+++ zq2.cpp
@@ -1,23 +1,22 @@
 #include "zq2.h"
 #include <stdio.h>  
 #include <pthread.h>
-extern "C"{
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-#include "php.h"
-#include "php_ini.h"
-#include "ext/standard/info.h"
-}  
+#include <cstring>
+#include <sstream>
+#include <stdlib.h>
+using namespace std;
 int i=0;
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+string zqfostr="";
 /*线程一*/  
 void *thread_1(void*)  
 {  
     while(true){
         pthread_mutex_lock(&mutex1);
         if(i>0){
-            php_printf("pthread_1 卖出的过火车票为：%d \n",i);
+            char zqqstr[256] = { 0 };
+            sprintf(zqqstr,"pthread_1 卖出的过火车票为:%d \n<br/>",i);
+            zqfostr +=(string)zqqstr;
             i--;
         }
         else
@@ -34,7 +33,9 @@ void *thread_2(void*)
     while(true){
         pthread_mutex_lock(&mutex1);
         if(i>0){
-            php_printf("pthread_2 卖出的过车票为：%d \n",i);
+            char zqqstr[256] = { 0 };
+            sprintf(zqqstr,"pthread_2 卖出的过火车票为:%d \n<br/>",i);
+            zqfostr +=(string)zqqstr;
             i--;
         }
         else
@@ -47,7 +48,7 @@ void *thread_2(void*)
 
 zqtests::zqtests():zqtest(20,30){
 }
-int zqtests::output1(int num){
+char* zqtests::output1(int num){
 	pthread_t id_1,id_2;  
     int ret;
     i=num;
@@ -55,23 +56,24 @@ int zqtests::output1(int num){
     ret=pthread_create(&id_1,NULL,thread_1,NULL);  
     if(ret!=0)  
     {  
-        php_printf("Create pthread_1 error!\n");  
-    return -1;  
+        printf("Create pthread_1 error!\n");  
+    return "err";  
     }  
 /*创建线程二*/  
      ret=pthread_create(&id_2,NULL,thread_2,NULL);  
     if(ret!=0)  
     {  
-        php_printf("Create pthread_2 error!\n");  
-    return -1;  
+        printf("Create pthread_2 error!\n");  
+    return "err";   
     }  
 /*等待线程结束*/  
     pthread_join(id_1,NULL);  
     pthread_join(id_2,NULL);  
     ret = pthread_mutex_destroy(&mutex1);
     if (ret != 0) {
-        php_printf("pthread_mutex_destroy error\n");
-        return -1;
+        printf("pthread_mutex_destroy error\n");
+        return "err"; 
     }
-	return x+y;
+
+	return (char *)zqfostr.c_str();
 }
